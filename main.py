1df83f23a57edb4a3655725f2339d41dbc725f85
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-21 12:14:30 +0300

    count numbers from task 3  function

--- main.py
+++ main.py
@@ -27,3 +27,34 @@ def count_digits(n: int):
             count += 1
 
     return count
+
+
+def count_numbers_condition(n: int):
+    count = 0
+
+    sum_digits = 0
+    local_n = n
+    while local_n > 0:
+        digit = local_n % 10
+        local_n //= 10
+        divs_count = 0
+        for i in range(1, digit + 1):
+            if digit % i == 0:
+                divs_count += 1
+
+        if divs_count <= 2:
+            sum_digits += digit
+
+    for i in range(1, n):
+        if n % i != 0 and (not coprime_numbers(i, n)) and coprime_numbers(i, sum_digits):
+            count += 1
+
+    return count
+
+
+def coprime_numbers(n: int, m: int) -> bool:
+    for i in range(2, int(max(n, m)**0.5) + 1):
+        if n % i == 0 and m % i == 0:
+            return False
+
+    return True
