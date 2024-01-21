d86e462e95bbc93536d9ded3537c38924fb2daf5
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-21 14:50:29 +0300

    main task-2-4

--- main1.py
+++ main1.py
@@ -14,3 +14,17 @@ def str_shuffle(s: str) -> str:
 
 def sort_chars_digits(s: str) -> str:
     return ''.join(sorted(s))
+
+
+if __name__ == '__main__':
+    tasks = {6: str_shuffle, 12: sort_chars_digits}
+    print('Привет! У меня есть задачи:')
+    for task_id, task_executor in tasks.items():
+        print(f'{task_id}. {task_executor.__name__}')
+
+    _id = input('Введи номер задачи: ')
+    arg = input('Введи аргумент к задаче: ')
+
+    r = tasks[int(_id)](arg)
+
+    print(f'Результат выполнения:\n{r}')
