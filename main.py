aaaf2faef0b867a715257c4aa8ca1cdc0ae3b9d0
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-21 11:26:25 +0300

    not prime divs sum  function

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 # Вариант 12
 
+def not_prime_divs_sum(n: int):
+    sm = 0
+    logger = []
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            for div in [i, n // i]:
+                count_divs = 0
+                for j in range(1, int(div**0.5) + 1):
+                    if div % j == 0:
+                        count_divs += 2
+
+                if count_divs > 2:
+                    sm += div
+                    logger.append(div)
+
+    print(sm, logger)
+
+
+not_prime_divs_sum(102)
