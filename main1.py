55c45af0d87f50b878db566f529a37d265fd924c
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-21 14:41:32 +0300

    sort chars digits  function

--- main1.py
+++ main1.py
@@ -10,3 +10,7 @@ def str_shuffle(s: str) -> str:
 
     words = list(map(lambda e: list(map(lambda el: chr(el), e)), words))
     return ' '.join(map(lambda e: ''.join(e), words))
+
+
+def sort_chars_digits(s: str) -> str:
+    return ''.join(sorted(s))
