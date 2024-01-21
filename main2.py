fb2f7c33e36fc215653766e860b417b5709b2331
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-21 15:59:07 +0300

    useless symbols  function

--- main2.py
+++ main2.py
@@ -5,3 +5,13 @@ def count_digits(s: str) -> int:
             count += 1
 
     return count
+
+
+def useless_symbols(s: str) -> str:
+    useless = ''
+    ru_seq = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    seq = set(map(lambda e: e.lower(), s))
+    for ru_char in ru_seq:
+        useless += ru_char if ru_char not in seq else ''
+
+    return useless
