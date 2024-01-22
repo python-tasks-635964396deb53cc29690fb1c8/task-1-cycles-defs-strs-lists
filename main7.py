083b4cd114134918b5fa3e9b752f0cb183929210
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-22 22:03:25 +0300

    list div index  function

--- main7.py
+++ main7.py
@@ -13,7 +13,7 @@ def data_input(s: str = '') -> list[int]:
 def reverse_min_max(numbers: list[int]) -> list[int]:
     mn, mx = min(numbers), max(numbers)
     m = list(map(lambda e: e[0], sorted([(numbers.index(mn), mn), (numbers.index(mx), mx)])))
-    return numbers[:m[0]+1] + numbers[m[1]-1:m[0]:-1] + numbers[m[1]:]
+    return numbers[:m[0] + 1] + numbers[m[1] - 1:m[0]:-1] + numbers[m[1]:]
 
 
 def max_max(numbers: list[int]) -> list[int]:
@@ -35,4 +35,9 @@ def frequency_seq(numbers: list[int]) -> list[int]:
     return [max(set(numbers), key=numbers.count) for _ in numbers]
 
 
-print(frequency_seq(data_input()))
+def list_div_index(numbers: list[int]) -> list[int]:
+    return [numbers[i] for i in range(len(numbers)) if i != 0 and numbers[i] % i == 0 and
+            numbers.count(numbers[i]) == 1]
+
+
+print(list_div_index(data_input()))
