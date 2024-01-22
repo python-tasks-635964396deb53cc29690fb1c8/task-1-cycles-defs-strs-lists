73cca9ee99ee37342542ac6fd845130a17edb75a
jEUGENEdev <genyamateyuk@gmail.com>
2024-01-22 21:40:24 +0300

    reverse min max  function

---
+++ main7.py
@@ -0,0 +1,19 @@
+def data_input(s: str = '') -> list[int]:
+    data = []
+
+    while 1:
+        line = input(s)
+        if not line:
+            break
+        data.append(int(line))
+
+    return data
+
+
+def reverse_min_max(numbers: list[int]) -> list[int]:
+    mn, mx = min(numbers), max(numbers)
+    m = list(map(lambda e: e[0], sorted([(numbers.index(mn), mn), (numbers.index(mx), mx)])))
+    return numbers[:m[0]+1] + numbers[m[1]-1:m[0]:-1] + numbers[m[1]:]
+
+
+print(reverse_min_max(data_input()))
